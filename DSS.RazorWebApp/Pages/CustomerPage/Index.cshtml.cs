73fd08086fc50816d9ce49fe6e57790740df8326
An Hoang <anhoang.code@gmail.com>
2024-06-19 13:10:22 +0700

    Search and Page

--- DSS.RazorWebApp/Pages/CustomerPage/Index.cshtml.cs
+++ DSS.RazorWebApp/Pages/CustomerPage/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DSS.Data.Models;
 using DSS.Business.Category;
+using System.Drawing.Printing;
 
 namespace DSS.RazorWebApp.Pages.NewFolder
 {
@@ -20,22 +21,33 @@ namespace DSS.RazorWebApp.Pages.NewFolder
         }
 
         public IList<Customer> Customer { get;set; } = default!;
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchBy, string search, int? pageNumber)
         {
             var result = await _business.GetAll();
             if (result != null && result.Status > 0 && result.Data != null)
             {
                 Customer = (List<Customer>)result.Data;
             }
+            if (search != null)
+            {
+                if (searchBy == "Name")
+                {
+                    Customer = Customer.Where(item => item.Name.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                else if (searchBy == "Address")
+                {
+                    Customer = Customer.Where(item => item.Address.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                else if (searchBy == "Phone")
+                {
+                    Customer = Customer.Where(item => item.Phone.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+            }
         }
-        //public void OnGet(string SearchString)
-        //{
-        //    if (!string.IsNullOrEmpty(SearchString))
-        //    {
-        //        //Customer = Customer.Where(c => c.Name.Contains(SearchString)).ToList();
-        //        Customer = Customer.Where(item => item.Name.Contains(SearchString, StringComparison.OrdinalIgnoreCase)).ToList();
-        //    }
-        //}
+    PageNumber = pageNumber ?? 1;
+    TotalPages = (int) System.Math.Ceiling(ExtraDiamond.ToList().Count / (double) PageSize);
+        ExtraDiamond = ExtraDiamond.Skip((PageNumber - 1) * PageSize).Take(PageSize).ToList();
+
     }
 
 }
